78d44a517e2c14767e2bda86f7c13dfad0d19f76
Daniel Latham <dlatham@openmailbox.org>
2015-10-24 19:43:47 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module's docstring summary line.
+This is a multi-line docstring. Paragraphs are separated with blank lines.
+Lines conform to 79-column limit.
+Module and packages names should be short, lower_case_with_underscores.
+See http://www.python.org/dev/peps/pep-0008/ for more PEP-8 details and
+http://wwd.ca/blog/2009/07/09/pep-8-cheatsheet/ for an up-to-date version
+of this cheatsheet.
+"""
+__author__ = 'D Latham'
